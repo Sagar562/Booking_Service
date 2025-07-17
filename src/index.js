b82e5f81e1334663937ee33f8d45f07ea7f3ab23
sagar <vyassagar4321@gmail.com>
2025-07-17 22:36:13 +0530

    added routes setup and body-parser, morgan package

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const { PORT } = require('./config/serverConfig');
+const apiRoutes = require('./routes/index');
+
+const app = express();
+
+const startServer = () => {
+    try {
+        app.use(bodyParser.json());
+        app.use(bodyParser.urlencoded( {extended: true }));
+
+        app.use('/api', apiRoutes);
+
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        })
+
+    } catch (error) {
+        console.log("Errr while starting server");
+        throw error;
+    }
+}
+
+startServer();
