0be45690302cbd8a7b82b1bb9a0683cfd40d7881
sagar <vyassagar4321@gmail.com>
2025-07-21 23:20:15 +0530

    fix: queue name

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -6,5 +6,6 @@ module.exports = {
     FLIGHT_SERVICE_PATH: process.env.FLIGHT_SERVICE_PATH,
     MESSAGE_BROKER_URL: process.env.MESSAGE_BROKER_URL, 
     EXCHANGE_NAME: process.env.EXCHANGE_NAME,
+    QUEUE_NAME: process.env.QUEUE_NAME,
     REMINDER_BINDING_KEY: process.env.REMINDER_BINDING_KEY
 }
